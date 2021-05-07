04ae6489d8eb3bc0be7b6e6927e58487a514bedb
bastians <bastians@live.de>
2021-05-07 16:40:53 +0200

    Move config to own config file

---
+++ config_example.php
@@ -0,0 +1,10 @@
+<?php
+
+// Rename this file to config.php and add the needed information below
+
+define('BINANCE_API_KEY', ''); // your binance API Key (enable futures)
+define('BINANCE_SECRET', ''); // your binance API Key secret
+define('TWITTER_API_KEY', ''); // twitter API Key
+define('TWITTER_SECRET', ''); // twitter API Key secret
+define('TARGET', '5'); /// set target in percent 
+define('AMOUNT', '6'); /// set the amount USDT (if empty it will take the available funds) - minimal order is 6 USDT
